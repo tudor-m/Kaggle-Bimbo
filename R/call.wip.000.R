4905d89972541579da65b50c6ca0a15d1400800c
tudor-m <tudor.morosan@yahoo.com>
2016-07-13 22:10:33 -0400

    Wip

--- R/call.wip.000.R
+++ R/call.wip.000.R
@@ -66,7 +66,7 @@ if (VALIDATION == 2) # short set of train/test for quick CV
   source(wip.R)
 }
 
-if (VALIDATION == 3) # short set of train/test for quick CV
+if (VALIDATION == 3) # CV
 {
   
   train.bak <- 
@@ -74,29 +74,31 @@ if (VALIDATION == 3) # short set of train/test for quick CV
           select = c("Semana","Agencia_ID","Canal_ID","Ruta_SAK","Cliente_ID","Producto_ID","Venta_uni_hoy","Venta_hoy","Dev_uni_proxima","Dev_proxima","Demanda_uni_equil"))
   
   nCli = 50000;
-  #nCli = 50000;
-  for (jBin in 1:ceiling(length(unique(train.bak$Cliente_ID))/nCli))
-  {
-  jMin = (jBin-1)*nCli+1
-  jMax = min(jBin*nCli,nrow(train.bak))
-  train <- train.bak
-  set.seed(2300)
+  nu_Cl = length(unique(train.bak$Cliente_ID))
+  n_Cl = length(train.bak$Cliente_ID)
 
-  trainCli = sample(unique(train$Cliente_ID),nCli*(1+jBin))[((jBin-1)*nCli+1):(jBin*nCli)]
-  trainWeeks = c(3,4,5,6,7)
-  testWeeks = c(8)
-  testWeeks2 = c(9)
-  idxTrain = which(train$Cliente_ID %in% trainCli & train$Semana %in% trainWeeks)
-  idxTest = which(train$Cliente_ID %in% trainCli & train$Semana %in% testWeeks)
-  idxTest2 = which(train$Cliente_ID %in% trainCli & train$Semana %in% testWeeks2)
-  test = train[idxTest,]
-  test2 = train[idxTest2,]
-  train = train[idxTrain,]
-  train$id = 1:nrow(train)
-  test$id = 1:nrow(test)
-  test2$id = 1:nrow(test2)
-  print(c("jBin: ",jBin))
-  source(wip.R)
+  for (jBin in 1:ceiling(nu_Cl/nCli))
+  {
+    jMin = (jBin-1)*nCli+1
+    jMax = min(jBin*nCli,nu_Cl)
+    train <- train.bak
+    set.seed(2300)
+
+    trainCli = sample(unique(train$Cliente_ID),length(unique(train$Cliente_ID)))[((jBin-1)*nCli+1):(jBin*nCli)]
+    trainWeeks = c(3,4,5,6,7)
+    testWeeks = c(8)
+    testWeeks2 = c(9)
+    idxTrain = which(train$Cliente_ID %in% trainCli & train$Semana %in% trainWeeks)
+    idxTest = which(train$Cliente_ID %in% trainCli & train$Semana %in% testWeeks)
+    idxTest2 = which(train$Cliente_ID %in% trainCli & train$Semana %in% testWeeks2)
+    test = train[idxTest,]
+    test2 = train[idxTest2,]
+    train = train[idxTrain,]
+    train$id = 1:nrow(train)
+    test$id = 1:nrow(test)
+    test2$id = 1:nrow(test2)
+    print(c("jBin: ",jBin))
+    source(wip.R)
   }
   remove(train.bak)
 }
