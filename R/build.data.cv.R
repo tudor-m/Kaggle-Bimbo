aa40f021c3efd68b81af80fec86c379099df61ed
tudor-m <tudor.morosan@yahoo.com>
2016-08-12 05:56:06 -0400

    Sync
    # Conflicts:
    #	R/assmbl.wip.000.R
    #	R/assmbl.wip.001.R
    #	R/build.data.cv-1.R
    #	R/call.assmbl.wip.001.R
    #	R/wip.002.R
    #	info-research-plan/Bimbo_Notes.xls

---
+++ R/build.data.cv.R
@@ -0,0 +1,38 @@
+library(data.table)
+
+# CV1 dataset
+DATABASE = "CV1"
+
+train.bak <- 
+  fread('../data/train.csv', header=TRUE,
+        select = c("Semana","Agencia_ID","Canal_ID","Ruta_SAK","Cliente_ID","Producto_ID","Venta_uni_hoy","Venta_hoy","Dev_uni_proxima","Dev_proxima","Demanda_uni_equil"))
+trainWeeks = c(3,4,5,6,7)
+testWeeks = c(8)
+
+nrow.train.bak = nrow(train.bak)
+clients.train.bak = unique(train.bak$Cliente_ID)
+products.train.bak = unique(train.bak$Producto_ID )
+agencies.train.bak = unique(train.bak$Agencia_ID)
+canals.train.bak = unique(train.bak$Canal_ID)
+routes.train.bak = unique(train.bak$Ruta_SAK)
+
+#train.bak$Demanda_uni_equil = log1p(train.bak$Demanda_uni_equil)
+
+trainData <- train.bak[which(train.bak$Semana %in% trainWeeks)]
+testData <- train.bak[which(train.bak$Semana %in% testWeeks)]
+remove(train.bak)
+trainData$id <- 1:nrow(trainData)
+testData$id <- 1:nrow(testData)
+gc()  # garbage collector
+
+train    = trainData
+test     = testData
+
+remove(trainData)
+remove(testData)
+gc()
+
+
+VERBOSE = 1
+source("wip.002.R")
+
