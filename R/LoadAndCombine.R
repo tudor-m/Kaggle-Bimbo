7088751b63b1a91d923d1608d4d44fa3ef30846a
tudor-m <tudor.morosan@yahoo.com>
2016-09-02 00:52:19 -0400

    wip

--- R/LoadAndCombine.R
+++ R/LoadAndCombine.R
@@ -8,7 +8,10 @@ if (1==0)
   inCoef = c(0.8,0.2)
 if (1==0)
   inCoef = c(0.85,0.15)
-inCoef = c(0.75,0.25)
+if (1==0)
+  inCoef = c(0.75,0.25)
+
+inCoef = c(0.7,0.3)
 
 for (i in 1:length(inDataFiles))
 {
@@ -35,6 +38,9 @@ if (1==0)
 if (1==0)
   write.csv(submitData[,.(id,Demanda_uni_equil)],"submit.LoadAndCombine.85_15.csv", row.names = FALSE)
 
-write.csv(submitData[,.(id,Demanda_uni_equil)],"submit.LoadAndCombine.75_25.csv", row.names = FALSE)
+if (1==0)
+  write.csv(submitData[,.(id,Demanda_uni_equil)],"submit.LoadAndCombine.75_25.csv", row.names = FALSE)
+
+write.csv(submitData[,.(id,Demanda_uni_equil)],"submit.LoadAndCombine.7_3.csv", row.names = FALSE)
 
 options(scipen = 0)
